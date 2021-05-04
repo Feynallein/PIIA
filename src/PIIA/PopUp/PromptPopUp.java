5be1f842ce169b22d1555f0aec43ee70132cbc69
Fanoa <loanrazafimbelo@gmail.com>
2021-05-04 19:11:29 +0200

    Pop up FIX

--- src/PIIA/PopUp/PromptPopUp.java
+++ src/PIIA/PopUp/PromptPopUp.java
@@ -1,8 +1,12 @@
 package PIIA.PopUp;
 
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.concurrent.Flow;
 
 public class PromptPopUp extends PopUpPane {
     private final String text;
@@ -11,14 +15,23 @@ public class PromptPopUp extends PopUpPane {
         super();
         this.text = text;
         display();
+        setHgap(10);
+        setVgap(30);
     }
 
     @Override
     void display() {
-        add(new Text(text), 0, 0);
+        FlowPane msg = new FlowPane();
+        msg.setAlignment(Pos.CENTER);
+        msg.getChildren().add(new Text(text));
+        add(msg, 0, 1);
 
         Button b = new Button("Ok");
         b.setOnMouseClicked(mouseEvent -> ((Stage) b.getScene().getWindow()).close());
-        add(b, 0, 1);
+
+        FlowPane fp = new FlowPane();
+        fp.setAlignment(Pos.CENTER);
+        fp.getChildren().add(b);
+        add(fp, 0, 2);
     }
 }
